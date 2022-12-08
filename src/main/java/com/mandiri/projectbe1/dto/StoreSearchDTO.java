5bbb5594e40e6bc9add9bbd62051d1eddaedf4eb
mcomacbook <mcomacbook@mcomackbookmbp5.supporting.corp.bankmandiri.co.id>
2022-12-08 14:52:14 +0700

    [IMPROVE] Making Store Search DTO

---
+++ src/main/java/com/mandiri/projectbe1/dto/StoreSearchDTO.java
@@ -0,0 +1,15 @@
+package com.mandiri.projectbe1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerSearchDTO {
+    private String storeAddress;
+    private String storeLicense;
+}
